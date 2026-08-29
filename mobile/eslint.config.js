2e43b3ad26c850ea4d559bb06620307c7c5bffdf
seunghye <sh0106015@gmail.com>
2026-08-29 23:49:15 +0900

    ci(mobile): eslint import 해석 규칙 비활성화 (경로 alias 오탐 방지)

--- mobile/eslint.config.js
+++ mobile/eslint.config.js
@@ -9,7 +9,12 @@ module.exports = defineConfig([
   },
   {
     rules: {
+      // 경로 alias(@/) + expo-router + RN 조합에서 오탐이 잦아 import 해석 규칙 비활성화
       'import/no-unresolved': 'off',
+      'import/namespace': 'off',
+      'import/default': 'off',
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
       // react-hooks v6 의 신규 실험적 규칙 - 제네릭 훅/템플릿 코드와 충돌하여 완화
       'react-hooks/set-state-in-effect': 'off',
       'react-hooks/use-memo': 'off',
